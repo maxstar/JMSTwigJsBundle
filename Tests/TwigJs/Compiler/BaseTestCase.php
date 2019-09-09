7c51144fca7c2f5be9ee14207c7970d9cc0a88aa
Oleg Andreyev <oleg.andreyev@ecentria.com>
2019-09-09 15:14:41 +0300

    added .travis.yml, improving phpunit support

--- Tests/TwigJs/Compiler/BaseTestCase.php
+++ Tests/TwigJs/Compiler/BaseTestCase.php
@@ -2,27 +2,34 @@
 
 namespace JMS\TwigJsBundle\Tests\TwigJs\Compiler;
 
-use JMS\TwigJsBundle\TwigJs\Compiler\TransFilterCompiler;
-use TwigJs\JsCompiler;
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\TwigBundle\Extension\AssetsExtension;
+use TwigJs\JsCompiler;
 
-abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
+if (\class_exists('\PHPUnit\Framework\TestCase')) {
+    abstract class BaseTestCaseAbstract extends TestCase
+    {
+    }
+} else {
+    abstract class BaseTestCaseAbstract extends \PHPUnit_Framework_TestCase
+    {
+    }
+}
+
+abstract class BaseTestCase extends BaseTestCaseAbstract
 {
     protected $env;
     protected $compiler;
 
-    protected function compile($source, $name = null)
-    {
+    protected function compile($source, $name = null) {
         return $this->env->compileSource($source, $name);
     }
 
-    protected function getNodes($source, $name = null)
-    {
+    protected function getNodes($source, $name = null) {
         return $this->env->parse($this->env->tokenize($source, $name));
     }
 
-    protected function setUp()
-    {
+    protected function setUp() {
         $this->env = $env = new \Twig_Environment();
         $env->addExtension(new \Twig_Extension_Core());
         $env->setCompiler($this->compiler = new JsCompiler($env));
